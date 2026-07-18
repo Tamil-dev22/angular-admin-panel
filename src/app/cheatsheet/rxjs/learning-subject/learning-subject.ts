cf5d71f3a682f7c402b477aaad76b8a786604115
Tamilarasan <tamilarasanpr22@gmail.com>
2026-07-18 12:56:52 +0530

    subject and types learned

--- src/app/cheatsheet/rxjs/learning-subject/learning-subject.ts
+++ src/app/cheatsheet/rxjs/learning-subject/learning-subject.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AsyncSubject, BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-learning-subject',
@@ -6,4 +7,80 @@ import { Component } from '@angular/core';
   templateUrl: './learning-subject.html',
   styleUrl: './learning-subject.scss',
 })
-export class LearningSubject {}
+export class LearningSubject {
+  scores: string[] = [];
+
+
+
+  log(message: string) {
+    this.scores.push(message);
+  }
+
+  clear() {
+    this.scores = []
+  }
+
+  runSubject() {
+    this.clear();
+    const score$ = new Subject<number>();
+
+    score$.next(20);
+    score$.next(30);
+
+    score$.subscribe(data => {
+      console.log(data)
+      this.log(`Subject:(User1: ${data})`)
+    })
+
+    score$.next(30);
+    score$.next(40);
+  }
+
+  runBehaviourSubject(){
+
+    this.clear();
+
+    const score$ = new BehaviorSubject<number>(0);
+
+    score$.next(10);
+    score$.next(20);
+
+    score$.subscribe((data)=>{
+      this.log(`BehaviourSubject: (User2 ${data})`)
+    });
+    score$.next(30);
+    score$.next(40);
+  }
+
+  runReplaySubject(){
+    this.clear();
+    const score$ =  new ReplaySubject<number>(3);
+    score$.next(10);
+    score$.next(20);
+    score$.subscribe((data)=>{
+    console.log(data);
+    this.log(`ReplaySubject: (User2 ${data})`);
+
+    });
+    score$.next(30);
+    score$.next(40);
+
+  }
+
+   runAsyncSubject(){
+    this.clear();
+    const score$ = new AsyncSubject<number>();
+    score$.next(10);
+    score$.next(20);
+    score$.subscribe((data)=>{
+      console.log(data);
+      this.log(`AsyncSubject:(User 3 ${data} )`)
+    })
+    score$.next(30);
+    score$.next(40);
+    score$.complete();
+   }
+
+
+
+}
